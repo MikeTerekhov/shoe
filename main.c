4418b69f614f77a184da5d1700c92697f228d48b
Mike Terekhov <Mike@MacBook-Pro-2.local>
2025-08-01 11:25:14 -0400

    what is your name?

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  printf("hello shoe!");
+  char username[100];
+  printf("Enter username: ");
+
+  fgets(username, sizeof(username), stdin);
+
+  // Remove the newline character that fgets includes
+  username[strcspn(username, "\n")] = '\0';
+
+  printf("Hello, %s!\n", username);
   return 0;
 }

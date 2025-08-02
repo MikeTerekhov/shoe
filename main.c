d1908b118b4aedea2efc55daa52f850bb5727d46
Mike Terekhov <Mike@MacBook-Pro-2.local>
2025-08-02 10:34:55 -0400

    options logic

--- main.c
+++ main.c
@@ -3,14 +3,39 @@
 #include <string.h>
 
 int main() {
-  char username[100];
-  printf("Enter username: ");
+  printf("-----------------------------------------\n");
+  printf("Welcome to Mike's Shoe Tracker App\n");
+  printf("-----------------------------------------\n");
+  printf("Here are the current options for what is available: \n");
+  printf("a.) Login \n");
+  printf("q.) Quit \n");
+  printf("-----------------------------------------\n");
 
-  fgets(username, sizeof(username), stdin);
+  char selection;
 
-  // Remove the newline character that fgets includes
-  username[strcspn(username, "\n")] = '\0';
+  printf("Please enter an option:  \n");
+  scanf("%c", &selection);
 
-  printf("Hello, %s!\n", username);
+  // this is for clearing the input buffer for scanf
+  while(getchar() != '\n');
+
+  switch(selection) {
+    case 'A' : case 'a' : {
+      char username[100];
+      printf("Enter username: ");
+      fgets(username, sizeof(username), stdin);
+      // Remove the newline character that fgets includes
+      username[strcspn(username, "\n")] = '\0';
+      printf("Hello, %s!\n", username);
+      break;
+    }
+    case 'Q' : case 'q':
+      printf("Goodbye!\n");
+      break;
+    default:
+      printf("Sorry, that is not an option!\n");
+  }
+
+
   return 0;
 }

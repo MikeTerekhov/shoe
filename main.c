a8d3d80d3cf3c24f0eb4e1e993ffb017f1bb0f0b
Mike Terekhov <terekhovhd@gmail.com>
2026-08-13 14:38:21 -0400

    integrate into main the sql db

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "db.h"
+
 int main() {
   printf("-----------------------------------------\n");
   printf("Welcome to Mike's Shoe Tracker App\n");
@@ -26,7 +28,27 @@ int main() {
       fgets(username, sizeof(username), stdin);
       // Remove the newline character that fgets includes
       username[strcspn(username, "\n")] = '\0';
-      printf("Hello, %s!\n", username);
+
+      sqlite3 *db = db_open("shoes.db");
+      if (!db) {
+        printf("Could not access the database.\n");
+        break;
+      }
+
+      int is_new = 0;
+      int user_id = db_get_or_create_user(db, username, &is_new);
+      db_close(db);
+
+      if (user_id < 0) {
+        printf("Something went wrong logging you in.\n");
+        break;
+      }
+
+      if (is_new) {
+        printf("Welcome, %s! You're all set up.\n", username);
+      } else {
+        printf("Hello again, %s!\n", username);
+      }
       break;
     }
     case 'Q' : case 'q':
